cd6ee42bfdcb7ad5ae75d55bfa8b3870693eaa2e
nathanial <nathanial28@gmail.com>
2026-04-24 21:34:57 -0600

    initial commit

--- scripts/python/libs/transparency/transparent-background/transparent_background/__init__.py
+++
@@ -1,8 +0,0 @@
-from transparent_background.Remover import Remover, console
-# transparent_background/__init__.py
-
-# Optional GUI import (don’t hard-fail if flet API differs)
-try:
-    from transparent_background.gui import gui
-except Exception:
-    gui = None
